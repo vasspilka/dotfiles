239f39068c074a66c8a651298bce36a5360edec3
Vasilis Spilka <vasspilka@gmail.com>
2016-01-24 02:14:46 +0200

    ignore init.vim

--- .vim/init.vim
+++
@@ -1,1 +0,0 @@
-/home/btd/.vimrc
--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ git submodule update
 echo "Dotfiles"
 for file in $(ls -A | grep "^\.[a-z]"| grep -v '.git$\|.gitmodules$\|\.gitignore$')
 do
+  rm -rf ~/$file
   ln -v -s `pwd`/$file ~
 done
 
 echo "Custom zsh files"
 for file in $(ls -A custom)
 do
+  rm -rf ~/.oh-my-zsh/custom/$file
   ln -v -s `pwd`/custom/$file ~/.oh-my-zsh/custom
 done
 for file in $(ls -A custom/plugins)
@@ -22,11 +24,7 @@ do
 done
 
 
-read -p "You use neovim yet? y/n"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  mkdir ~/.config
-  rm -rf ~/.config/nvim
-  ln -s ~/.vim ~/.config/nvim
-  ln -s ~/.vimrc ~/.config/nvim/init.vim
-fi
+mkdir ~/.config
+rm -rf ~/.config/nvim
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
