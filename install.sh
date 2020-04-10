d8253ccbd8c7fca08815566a0c33f8d3fff14e71
Vasilis Spilka <vasspilka@gmail.com>
2020-04-10 13:49:22 +0200

    remove submodules and update clean vimrc

--- install.sh
+++ install.sh
@@ -15,7 +15,6 @@ install_dev_tools() {
 
   # Starship prompt ++
   curl -fsSL https://starship.rs/install.sh | bash
-  
 
   # Zsh stuff
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -24,31 +23,19 @@ install_dev_tools() {
   git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/gusaiani/elixir-oh-my-zsh.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/elixir
-
-
 }
 
-{
-git clone https://github.com/syl20bnr/spacemacs ~/.spacemacs.d
-git clone https://github.com/hlissner/doom-emacs ~/.doom-emacs
-doom install
+install_emacs() {
+  git clone https://github.com/syl20bnr/spacemacs ~/.spacemacs.d
+  git clone https://github.com/hlissner/doom-emacs ~/.doom-emacs
+  doom install
 
-wget -O ~/.emacs https://raw.githubusercontent.com/plexus/chemacs/master/.emacs
+  wget -O ~/.emacs https://raw.githubusercontent.com/plexus/chemacs/master/.emacs
 }  > ~/.dotinstall.tmp
 
 echo "Dotfiles"
-for file in $(ls -A | grep "^\.[a-z]"| grep -v '.git$\|.gitmodules$\|\.gitignore$')
+for file in $(ls -A | grep "^\.[a-z]"| grep -v '.git$\|.gitmodules$\|\.gitignore$|\.config$')
 do
   rm -rf ~/$file
   ln -v -s `pwd`/$file ~
 done
-
-# echo "Custom zsh files"
-# for file in $(ls -A custom)
-# do
-#   ln -v -s `pwd`/custom/$file ~/.oh-my-zsh/custom
-# done
-# for file in $(ls -A custom/plugins)
-# do
-#   ln -v -s `pwd`/custom/plugins/$file ~/.oh-my-zsh/custom/plugins
-# done
