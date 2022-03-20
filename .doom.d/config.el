6818717072ace09c1359f3252dad0ceba4ff3942
Vasilis Spilka <vasspilka@gmail.com>
2022-03-20 17:50:24 +0100

    update doom

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,18 +2,16 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-;;
-;;
+
 
 ;; Setup ENV for MAC
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
+;; (when (memq window-system '(mac ns x))
+;;   (exec-path-from-shell-initialize))
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Vasilis Spilka"
       user-mail-address "vasspilka@gmail.com"
-      projectile-project-search-path '("~/Work")
       )
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
@@ -26,26 +24,69 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Source Code Pro" :size 18))
-(setq display-line-numbers-type nil)
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dracula)
+(setq doom-font (font-spec :family "Source Code Pro" :size 16))
+(setq display-line-numbers-type nil)
+(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+;; (use-package! tree-sitter
+;;   :init
+;;   (defface tree-sitter-hl-face:warning
+;;     '((default :inherit font-lock-warning-face))
+;;     "Face for parser errors"
+;;     :group 'tree-sitter-hl-faces)
 
-;; Sort keys
-(setq which-key-sort-order 'which-key-key-order-alpha)
+;;   (defun hook/tree-sitter-common ()
+;;     (unless font-lock-defaults
+;;       (setq font-lock-defaults '(nil)))
+;;     (setq tree-sitter-hl-use-font-lock-keywords nil)
+;;     (tree-sitter-mode +1)
+;;     (tree-sitter-hl-mode +1))
 
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;;   (defun hook/elixir-tree-sitter ()
+;;     (require 'f)
+;;     (require 's)
+
+;;     (setq tree-sitter-hl-default-patterns
+;;      (read
+;;       (concat
+;;        "["
+;;        (s-replace "#match?" ".match?"
+;;                   (f-read-text (expand-file-name "~/tree-sitter-elixir/queries/highlights.scm")))
+;;        "]")))
+;;     (hook/tree-sitter-common))
+
+;;   :hook ((elixir-mode . hook/elixir-tree-sitter))
+;;   :custom-face
+;;   (tree-sitter-hl-face:operator ((t)))
+;;   (tree-sitter-hl-face:variable ((t)))
+;;   (tree-sitter-hl-face:function.method.call ((t)))
+;;   (tree-sitter-hl-face:property ((t))))
+;;
+(use-package! tree-sitter
+  :config
+  (require 'tree-sitter-langs)
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+
+(use-package! tree-sitter-langs
+  :after tree-sitter
+  :config
+  (add-to-list 'tree-sitter-major-mode-language-alist '(elixir-mode . elixir)))
+
+
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 
 (defun elixir-append-inspect ()
   (interactive)
@@ -60,12 +101,6 @@
 ;; Configure exunit
 (use-package! exunit)
 
-;; Taken off https://gist.github.com/darioghilardi/f3415a3d70d4fee5a20bfad862534a37
-;; Elixir configuration
-
-;; I should do instead
-;; (add-to-list 'exec-path "${elixir-lsp}/bin")
-;; (setq lsp-clients-elixir-server-executable "elixir-ls")
 
 ;; Setup some keybindings for exunit and lsp-ui
 (map! :mode elixir-mode
@@ -75,23 +110,12 @@
       :desc "Toggle Test" :nve  "cT"    #'exunit-toggle-file-and-test
       :desc "Inspect" :nve  "cI"    #'elixir-append-inspect)
 
-(after! lsp-mode
-  (dolist (match
-           '("[/\\\\].direnv$"
-             "[/\\\\]node_modules$"
-             "[/\\\\]deps"
-             "[/\\\\]priv"
-             "[/\\\\]build"
-             "[/\\\\]_build"))
-    (add-to-list 'lsp-file-watch-ignored match)))
-
 (keychain-refresh-environment)
 
-
 ;; Here are some additional functions/macros that could help you configure Doom:
-;;
+;
 ;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
+;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
@@ -99,8 +123,8 @@
 ;; - `map!' for binding new keys
 ;;
 ;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
 ;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,33 +1,16 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here, run 'doom sync' on
-;; the command line, then restart Emacs for the changes to take effect.
-;; Alternatively, use M-x doom/reload.
-;;
-;; WARNING: Disabling core packages listed in ~/.emacs.d/core/packages.el may
-;; have nasty side-effects and is not recommended.
-
-
-;; All of Doom's packages are pinned to a specific commit, and updated from
-;; release to release. To un-pin all packages and live on the edge, do:
-;(unpin! t)
-
-;; ...but to unpin a single package:
-;(unpin! pinned-package)
-;; Use it to unpin multiple packages
-;(unpin! pinned-package another-pinned-package)
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-(package! fish-mode)
-(package! exec-path-from-shell)
-(package! keychain-environment)
-(package! easy-jekyll)
+;(package! some-package)
 
-
-;; To install a package directly from a particular repo, you'll need to specify
-;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; To install a package directly from a remote git repo, you must specify a
+;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
@@ -39,10 +22,14 @@
 ;  :recipe (:host github :repo "username/repo"
 ;           :files ("some-file.el" "src/lisp/*.el")))
 
-;; If you'd like to disable a package included with Doom, for whatever reason,
-;; you can do so here with the `:disable' property:
-(package! alchemist :disable t)
-(package! evil-escape :disable t)
+(package! exec-path-from-shell)
+(package! keychain-environment)
+(package! tree-sitter)
+(package! tree-sitter-langs)
+
+;; If you'd like to disable a package included with Doom, you can do so here
+;; with the `:disable' property:
+;(package! builtin-package :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. These will inherit the rest of its recipe
@@ -54,3 +41,17 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+;; Use `:pin' to specify a particular commit to install.
+;(package! builtin-package :pin "1a2b3c4d5e")
+(package! alchemist :disable t)
+(package! evil-escape :disable t)
+
+
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+;(unpin! pinned-package)
+;; ...or multiple packages
+;(unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+;(unpin! t)
